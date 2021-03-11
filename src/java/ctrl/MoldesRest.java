3c657c8621e1a6366630233f76e0a8f3a6d9a7a7
Juan Pablo Chica <juanpchica@hotmail.co>
2021-03-11 15:21:34 -0500

    Http headers changed

--- src/java/ctrl/MoldesRest.java
+++ src/java/ctrl/MoldesRest.java
@@ -29,18 +29,12 @@ public class MoldesRest {
     
     @PUT
     @Path("/actualizar")
-    @Consumes(value= MediaType.APPLICATION_JSON)
-    @Produces(value = MediaType.APPLICATION_JSON)
     public Response actualizar(Molde molde){
         
         boolean resp = MoldesCtrl.updateMolde(molde);
         
         if(resp){
-            return Response.status(200)
-                .header("Access-Control-Allow-Origin", "http://localhost:3000")
-                .header("Access-Control-Allow-Credentials", true)
-                .header("Access-Control-Allow-Headers","origin, content-type, accept")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS").build();
+            return Response.ok(molde).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Credentials", "true").header("Access-Control-Allow-Headers","origin, content-type, accept, authorization").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD").build();
         }else{
             return Response.status(Response.Status.BAD_REQUEST).entity("User not found").header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Credentials", "true").header("Access-Control-Allow-Headers","origin, content-type, accept, authorization").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD").build();
         }
