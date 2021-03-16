cf09abc4d6ec17278a6729413ea5212821e83af8
Juan Pablo Chica <juanpchica@hotmail.co>
2021-03-16 14:39:06 -0500

    Jersey lib modified and new libraries added

--- src/java/ctrl/MoldesRest.java
+++ src/java/ctrl/MoldesRest.java
@@ -2,6 +2,7 @@ package ctrl;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,22 +25,32 @@ public class MoldesRest {
     @GET
     public Response getVista(@QueryParam("id") String id) {
         ServiciosBD.conectar();
-        return Response.ok(MoldesCtrl.getMoldes(id)).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Credentials", "true").header("Access-Control-Allow-Headers","origin, content-type, accept, authorization").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD").build();
+        return Response.ok(MoldesCtrl.getMoldes(id)).build();
     }
      
     @PUT
     @Consumes(value= MediaType.APPLICATION_JSON)
-    @Produces(value= MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON)
     @Path("/actualizar") 
     public Response actualizar(Molde molde){
-        
-            return Response.ok(MoldesCtrl.updateMolde(molde))
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .build();
+            Molde m = molde;
+            return Response.ok(MoldesCtrl.updateMolde(m)).build();
        
     }
+    
+//    @PUT
+//    @Consumes(value= MediaType.APPLICATION_JSON)
+//    @Produces(value= MediaType.APPLICATION_JSON)
+//    @Path("/actualizar") 
+//    public Response actualizar(Molde molde){
+//            Molde m = molde;
+//            return Response.ok(MoldesCtrl.updateMolde(molde))
+//                    .header("Access-Control-Allow-Origin", "*")
+//                    .header("Access-Control-Allow-Credentials", "true")
+//                    .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
+//                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+//                    .header("Access-Control-Max-Age", "1209600")
+//                    .build();
+//       
+//    }
 }
